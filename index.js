3977a659b8db52c88d6f1aaba0415a6e7f579144
Filip FICO <fitz993m@gmail.com>
2021-06-21 18:23:44 +0200

    new commit to 2nd feature branch

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 4000;
+console.log('something');
 
 app.listen(port, () => console.log(`Listening on port${port}`));
